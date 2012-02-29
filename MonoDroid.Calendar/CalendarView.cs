05be975beabb1625db5c31e76a727bf94e83b67c
Tomasz Cielecki <tomasz@ostebaronen.dk>
2012-02-29 01:20:39 +0100

    Added license to head.

--- MonoDroid.Calendar/CalendarView.cs
+++ MonoDroid.Calendar/CalendarView.cs
@@ -1,3 +1,23 @@
+/*
+ * Ported C# code:
+ * Copyright (C) 2012 Tomasz Cielecki <tomasz@ostebaronen.dk>
+ * 
+ * Original Java code: https://code.google.com/p/android-calendar-view/source/browse/trunk/src/com/exina/android/calendar/CalendarView.java
+ * Copyright (C) 2011 Chris Gao <chris@exina.net>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 using System;
 
 using Android.Widget;
